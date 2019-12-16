11ba60b6139100b64fb51623be787751a0da332c
Dave Horton <daveh@beachdognet.com>
2019-12-16 16:04:46 -0500

    more tests

--- test/lcr.js
+++ test/lcr.js
@@ -1,6 +1,8 @@
 const test = require('tape').test ;
 const config = require('config');
 const mysqlOpts = config.get('mysql');
+const {execSync} = require('child_process');
+const pwd = process.env.TRAVIS ? '' : '-p$MYSQL_ROOT_PASSWORD';
 
 process.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
@@ -28,6 +30,22 @@ test('lcr tests', async(t) => {
     catch (err) {
       t.ok(err.message === 'no matching lcr route', 'throws \'no matching lcr route\' when called number does not match anything');
     }
+
+    // remove all lcr configuration
+    let stdout = execSync(`mysql -h localhost -u root ${pwd} -D jambones_test -e "delete from lcr_carrier_set_entry"`);
+    stdout = execSync(`mysql -h localhost -u root ${pwd} -D jambones_test -e "delete from lcr_routes"`);
+
+    gateways = await performLcr('4412838238238');
+    //console.log(`gateways: ${JSON.stringify(gateways)}`);
+    t.ok(gateways.length === 2, 'when lcr is not configured at all, return a randomly shuffled list of outbound gateways');
+
+    // clear data and insert data with multiple carriers in same priority
+    execSync(`mysql -h localhost -u root ${pwd} -D jambones_test < ${__dirname}/db/jambones-sql.sql`);
+    execSync(`mysql -h localhost -u root ${pwd} -D jambones_test < ${__dirname}/db/populate-test-data2.sql`);
+
+    gateways = await performLcr('4412838238238');
+    //console.log(`gateways: ${JSON.stringify(gateways)}`);
+    t.ok(gateways.length === 8 && gateways[7] === '10.10.10.10:5060', 'handles multiple carriers');
 
     t.end();
   }
